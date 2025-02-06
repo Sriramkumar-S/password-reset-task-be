d89dfefd61624439746c23031ab029da78638c6c
Sriramkumar-S <sriramsrk005@gmail.com>
2025-02-06 10:01:46 +0530

    Updated mail key pwd and added code to keep server up and running

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import { connectToMongoose } from './db-utils/mongoose-connection.js';
 import { connectToDB } from './db-utils/mongoDB-connection.js';
 import userRouter from './routes/users.js';
+import { job } from './serverRestart.js';
 
 const server = express();
 
@@ -15,6 +16,9 @@ const PORT = 4500;
 
 await connectToDB();
 await connectToMongoose();
+
+// Restart Job after 14 minutes
+job.start();
 
 server.listen(PORT, () => {
     console.log("Server listening on ", PORT);
